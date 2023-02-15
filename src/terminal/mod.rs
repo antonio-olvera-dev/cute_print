bfde080b644df3d8096b6f8ac1aff1f10e2a9d98
Antonio Olvera <antonioolvera1995@gmail.com>
2023-02-15 23:11:16 +0100

    added unsupported os

--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -30,4 +30,34 @@ pub mod windows;
 #[cfg(target_os = "windows")]
 pub use windows::get_terminal_width;
 
+#[cfg(not(any(
+    target_os = "linux",
+    target_os = "android",
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "bitrig",
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "solaris",
+    target_os = "windows"
+)))]
+pub mod unsupported;
+
+#[cfg(not(any(
+    target_os = "linux",
+    target_os = "android",
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "bitrig",
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "solaris",
+    target_os = "windows"
+)))]
+pub use unsupported::get_terminal_width;
+
 mod test;
---
+++ src/terminal/unsupported.rs
@@ -0,0 +1,6 @@
+/// Gets the width of the current terminal.
+/// # Returns
+/// Returns a Option<u16>.
+pub fn get_terminal_width() -> Option<u16> {
+    return None;
+}
