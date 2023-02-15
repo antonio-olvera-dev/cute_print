98b3945737578902370e30cefc65eb79ef823652
Antonio Olvera <antonioolvera1995@gmail.com>
2023-02-15 23:25:16 +0100

    deleted unnecessary test

--- src/terminal/test/mod.rs
+++ src/terminal/test/mod.rs
@@ -1,16 +1,1 @@
-#[cfg(any(
-    target_os = "linux",
-    target_os = "android",
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "bitrig",
-    target_os = "dragonfly",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "openbsd",
-    target_os = "solaris"
-))]
-mod unix_test;
-
-#[cfg(target_os = "windows")]
-mod windows_test;
+mod terminal_test;
